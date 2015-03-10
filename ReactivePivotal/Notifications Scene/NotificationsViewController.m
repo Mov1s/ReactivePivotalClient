4192ddf371a4eadf82627976774b9859dac67f35
Ryan Popa <ryanp@recsolu.com>
2015-03-10 09:32:35 -0500

    Showing notifications in tableview

--- ReactivePivotal/Notifications Scene/NotificationsViewController.m
+++ ReactivePivotal/Notifications Scene/NotificationsViewController.m
@@ -15,6 +15,9 @@
 /** The view model for this view controller */
 @property (nonatomic, weak) IBOutlet NotificationsViewModel *viewModel;
 
+/** The table view to show notifications in */
+@property (nonatomic, weak) IBOutlet UITableView *tableView;
+
 @end
 
 @implementation NotificationsViewController
@@ -24,9 +27,29 @@
     [super viewDidLoad];
     
     //Bind notifications
+    @weakify(self);
     [RACObserve(self.viewModel, notifications) subscribeNext: ^(id x) {
-        //Refresh table
+        @strongify(self);
+        [self.tableView reloadData];
     }];
+}
+
+#pragma mark - UITableView Datasource
+- (NSInteger)tableView: (UITableView *)tableView numberOfRowsInSection: (NSInteger)section
+{
+    return self.viewModel.notifications.count;
+}
+
+- (UITableViewCell *)tableView: (UITableView *)tableView cellForRowAtIndexPath: (NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier: @"notifcationCell"];
+    NSDictionary *notification = self.viewModel.notifications[indexPath.row];
+    
+    //Configure cell
+    cell.textLabel.text = notification[@"message"];
+    cell.detailTextLabel.text = notification[@"context"];
+    
+    return cell;
 }
 
 @end
